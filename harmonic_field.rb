b6db0b3f5aa7b97ba9dbd6104affec12d389d51f
Matheus Silva Santos de Oliveira <oliveira.matheussilva@outlook.com>
2019-05-22 17:03:58 -0300

    [WIP] Start to query in DynamoDB

--- harmonic_field.rb
+++ harmonic_field.rb
@@ -64,39 +64,11 @@ def acorde(params)
 end
 
 def search_acorde_in_harmonic_field(acordes)
+  table = Aws::DynamoDB::Table.new("harmonic_fields")
+  options = { key_condition_expression: "chord = :chord", expression_attribute_values: { ":chord" => acorde } }
+  table.query(options).items
   HARMONIC_FIELD.map do |harmonic_field|
     puts "[LOGGER] #{(harmonic_field[:acordes] & acordes)}"
     harmonic_field[:name] if (acordes - harmonic_field[:acordes]).empty?
   end.compact
 end
-
-HARMONIC_FIELD = [
-  {
-    name: "campo harmônico de dó maior",
-    acordes: ["C7M", "Dm7", "Em7", "F7M", "G7", "Am7", "Bm7(b5)"]
-  },
-  {
-    name: "campo harmônico de ré maior",
-    acordes: ["D7M", "Em7", "F#m7", "G7M", "A7", "Bm7", "C#m7(b5)"]
-  },
-  {
-    name: "campo harmônico de mi maior",
-    acordes: ["E7M", "F#m7", "G#m7", "A7M", "B7", "C#m7", "D#m7(b5)"]
-  },
-  {
-    name: "campo harmônico de fá maior",
-    acordes: ["F7M", "Gm7", "Am7", "Bb7M", "C7", "Dm7", "E#m7(b5)"]
-  },
-  {
-    name: "campo harmônico de sol maior",
-    acordes: ["G7M", "Am7", "Bm7", "C7M", "D7", "Em7", "F#m7(b5)"]
-  },
-  {
-    name: "campo harmônico de lá maior",
-    acordes: ["A7M", "Bm7", "C#m7", "D7M", "E7", "F#m7", "G#m7(b5)"]
-  },
-  {
-    name: "campo harmônico de si maior",
-    acordes: ["B7M", "C#m7", "D#m7", "E7M", "F#7", "G#m7", "A#m7(b5)"]
-  }
-]
